94e26cc3dc2abd48943af75dd3cd4997ba93263e
Mudiey <132483185+Mudiey@users.noreply.github.com>
2023-05-20 15:48:22 +0100

    Add files via upload

---
+++ socials.js
@@ -0,0 +1,19 @@
+function redirectToSocialMedia(platform) {
+    switch (platform) {
+      case "facebook":
+        window.location.href = "https://web.facebook.com/Mahmud51/";
+        break;
+      case "twitter":
+        window.location.href = "https://twitter.com/mahmud_b_k";
+        break;
+      case "instagram":
+        window.location.href = "https://www.instagram.com/mahmud.b.k/";
+        break;
+      case "tiktok":
+        window.location.href = "https://www.tiktok.com/@_m.b.k__";
+        break;
+      default:
+        break;
+    }
+  }
+  
